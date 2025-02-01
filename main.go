9e9cf45da557d7e36f5b5f6d7ddbe2e407e6c1b8
cpmachado <cpmachado@protonmail.com>
2025-02-01 23:03:43 +0000

    Annotate current source code

--- main.go
+++ main.go
@@ -7,10 +7,13 @@ import (
 )
 
 const (
-	DefaultN          = 8
+	// DefaultN is the default side of board
+	DefaultN = 8
+	// DefaultOutputFile is the default output file, in which "" is os.Stdout
 	DefaultOutputFile = ""
 )
 
+// Version holds the version of the current software based on compilation
 var Version string = "unknown"
 
 func main() {
@@ -21,7 +24,7 @@ func main() {
 	)
 	flag.IntVar(&n, "n", DefaultN, "Length of side of chess board")
 	flag.StringVar(&outputFile, "o", DefaultOutputFile, "Output file")
-	flag.BoolVar(&v, "v", false, "Display version")
+	flag.BoolVar(&v, "v", false, "Display version, and exit if true")
 	flag.Usage = Usage
 	flag.Parse()
 
@@ -32,10 +35,12 @@ func main() {
 	fmt.Println(n, outputFile, v)
 }
 
+// printVersion Prints the version command
 func printVersion() {
 	fmt.Printf("%s-%s Copyright (C) 2025 Carlos Pinto Machado\n", os.Args[0], Version)
 }
 
+// Usage is a function to replace the default provided in the flag package
 func Usage() {
 	prog := os.Args[0]
 	fmt.Fprintf(flag.CommandLine.Output(), "%s is a program to solve the N queen puzzle\n", prog)
